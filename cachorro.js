76ce90af5f2f9cd8769e2039e1e58f4c60beb022
andreluizborgess <andreluizborges1995@gmail.com>
2023-08-22 10:46:33 -0300

    first commit

---
+++ cachorro.js
@@ -0,0 +1,18 @@
+"use strict";
+class cachorro {
+    constructor(nome, raca, idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+    apresentar() {
+        return "oi, eu sou" + this.nome + "um" + this.raca + "de" + this.idade + "anos";
+    }
+    apresentarPara(humano) {
+        return "olá" + humano + "eu sou" + +this.raca + "de" + this.idade + "anos";
+    }
+}
+let rex = new cachorro("rex", "labrador", 3);
+let thor = new cachorro("thor", "bulldog", 2);
+console.log(rex.nome);
+console.log(thor.nome);
---
+++ cachorro.ts
@@ -0,0 +1,24 @@
+class cachorro{
+    nome: string;
+    raca:string;
+    idade: number;
+    constructor(nome: string, raca: string, idade: number){
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+
+    apresentar(){
+        return "oi, eu sou" + this.nome + "um" + this.raca + "de" + this.idade + "anos";
+    }
+    apresentarPara(humano:string){
+
+        return "olá"  + humano + "eu sou" + + this.raca + "de" + this.idade + "anos";
+        }
+}
+
+let rex = new cachorro("rex", "labrador" , 3)
+let thor =new cachorro("thor", "bulldog" , 2)
+console.log(rex.nome);
+console.log(thor.nome);
+
